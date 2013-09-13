7e2eb6ac1afde65dc101db54c6f8829c52c9629c
Paul Fenwick <pjf@perltraining.com.au>
2013-09-13 10:41:04 +0000

    Simple cache for exobrain

---
+++ lib/App/Exobrain/Cache.pm
@@ -0,0 +1,16 @@
+package App::Exobrain::Cache;
+
+use v5.10.0;
+use strict;
+use warnings;
+
+use parent qw(CHI);
+
+__PACKAGE__->config({
+    defaults => {
+        driver   => 'File',
+        root_dir => "$ENV{HOME}/.exobrain/cache",
+    }
+});
+
+1;
