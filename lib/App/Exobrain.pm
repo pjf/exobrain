60ee5b5fdc5befbc4e82a96bc28f7a042f3c1b03
Paul Fenwick <pjf@perltraining.com.au>
2013-09-19 04:07:35 +0000

    Helper methods for getting sub/pub access.

--- lib/App/Exobrain.pm
+++ lib/App/Exobrain.pm
@@ -18,9 +18,27 @@ has 'config' => (
     builder => '_build_config',
 );
 
-sub _build_config {
-    return App::Exobrain::Config->new;
-};
+# Pub/Sub interfaces to our bus. These don't get generated unless
+# our end code actually asks for them. Many things will only require
+# one, or will use higher-level functions to do their work.
+
+has 'pub' => (
+    is => 'ro',
+    isa => 'App::Exobrain::Bus',
+    builder => '_build_pub',
+    lazy => 1,
+);
+
+has 'sub' => (
+    is => 'ro',
+    isa => 'App::Exobrain::Bus',
+    builder => '_build_sub',
+    lazy => 1,
+);
+
+sub _build_config { return App::Exobrain::Config->new; };
+sub _build_pub    { return App::Exobrain::Bus->new(type => 'PUB') }
+sub _build_sub    { return App::Exobrain::Bus->new(type => 'SUB') }
 
 =for Pod::Coverage BUILD DEMOLISH
 
