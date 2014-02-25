457b57485335b231d3732751ddbf1fccaced0e99
Paul Fenwick <pjf@perltraining.com.au>
2014-02-24 19:36:14 -0500

    Added Run class.

--- lib/Exobrain/Source/Run.pm
+++ lib/Exobrain/Source/Run.pm
@@ -1,5 +1,7 @@
 package Exobrain::Source::Run;
 use Moose::Role;
+use Method::Signatures;
+use Try::Tiny;
 
 # ABSTRACT: Role for sources which run 'continously'
 
@@ -7,5 +9,24 @@ with 'Exobrain::Source';
 
 requires('run');
 excludes('poll');
+
+=method start
+
+Called automatically by exobrain. This just wraps the C<run> method,
+and signals an error should that method ever return.
+
+=cut
+
+method start() {
+    $self->run;
+
+    my $class = ref($self);
+    my $message = "Error: $class exited run() method unexpectedly.";
+
+    try { $self->exobrain->notify($message, priority => 1); };
+
+    die $message;
+
+}
 
 1;
