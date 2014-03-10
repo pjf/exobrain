41ee4e462fe0ba837c78e3ba0a3d47a6b5f3eeca
Paul Fenwick <pjf@perltraining.com.au>
2014-03-11 00:52:25 +1100

    Expanded attributes provided by Exobrain::Agent.

--- lib/Exobrain/Agent.pm
+++ lib/Exobrain/Agent.pm
@@ -1,9 +1,34 @@
 package Exobrain::Agent;
 use Moose::Role;
 use Exobrain;
-use Exobrain::Cache;
-use Method::Signatures;
 use Exobrain::Types qw(Exobrain);
+
+# ABSTRACT: Agent role for Exobrain agents
+# VERSION
+
+=head1 SYNOPSIS
+
+    use Moose;
+    with 'Exobrain::Agent';
+
+=head1 DESCRIPTION
+
+This role provides a number of utility methods and attributes to
+Exobrain agents. Most agents will prefer to use the
+L<Exobrain::Agent::Poll> or L<Exobrain::Agent::Run> roles
+instead, and which in turn consume this role and hence provide
+its functionality.
+
+Where possible, all attributes provided by this role are lazily
+generated, including lazily loading modules.
+
+=method exobrain
+
+    $self->exobrain->watch_loop(...);
+
+Returns an L<Exobrain> object.
+
+=cut
 
 has exobrain => (
     is => 'ro',
@@ -14,10 +39,21 @@ has exobrain => (
 
 sub _build_exobrain { return Exobrain->new; }
 
-# Our component name is by default the same as the class name with
-# 'Exobrain::Agent::' stripped off. However different modules can
-# declare themselves to be part of the same component, thereby sharing
-# config and cache.
+=method component
+
+    my $component = $self->component;
+
+Exobrain components all share the same configuration and cache,
+and are often packaged as separate distributions. This method
+returns the current component name.
+
+By default this is the same as the class name with
+'Exobrain::Agent::' stripped off. Agents and roles which are
+part of a specific component should override this attribute
+(or supply their own default).
+
+=cut
+
 
 has component => (
     is => 'ro',
@@ -36,6 +72,16 @@ method _build_component() {
     return $component;
 }
 
+=method config
+
+    my $user = $self->config->{user};
+
+Returns the config for our current component.
+Use C<$self->exobrain->config> instead if you want the I<top-level>
+config.
+
+=cut
+
 has config => (
     is => 'ro',
     lazy => 1,
@@ -43,12 +89,20 @@ has config => (
 );
 
 # By using the component name as the heading for our config, it's easy
-# to clearly tell which config belongs where. Consumers can always
-# call $exobrain->config if they want to access the top-level one.
+# to clearly tell which config belongs where.
 
 method _build_config() {
     return $self->exobrain->config->{ $self->component };
 }
+
+=method cache
+
+    my $cache = $self->cache;
+
+Returns an L<Exobrain::Cache> object, with a namespace of
+C<$self->component>.
+
+=cut
 
 has cache => (
     is => 'ro',
@@ -57,7 +111,27 @@ has cache => (
 );
 
 method _build_cache() {
+    eval "use Exobrain::Cache; 1;" or die $@;   # Lazy load module
     return Exobrain::Cache->new( namespace => $self->component );
+}
+
+=method json
+
+    my $json = $self->json;
+
+Returns a L<JSON::Any> object.
+
+=cut
+
+has json => (
+    is => 'ro',
+    lazy => 1,
+    builder => '_build_json',
+);
+
+method _build_json() {
+    eval "use JSON::Any; 1;" or die $@;
+    return JSON::Any->new;
 }
 
 1;
--- t/agent.t
+++ t/agent.t
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+use Test::More;
 
 package Exobrain::Agent::Testing;
 
@@ -8,6 +9,25 @@ use Exobrain::Test;
 
 with 'Exobrain::Agent';
 
-ok(1, "Can consume Exobrain::Agent");
+::ok(1, "Can consume Exobrain::Agent");
+
+package main;
+
+my $agent = Exobrain::Agent::Testing->new;
+
+isa_ok($agent->exobrain,  'Exobrain');
+isa_ok($agent->json,      'JSON::Any');
+is(    $agent->component, 'Testing');
+
+TODO: {
+    local $TODO = "Because this returns a sub-hash, of our config, it's not an obejct";
+    # We should probably just be checking for a known key.
+    isa_ok($agent->config,    'Exobrain::Config');
+}
+
+TODO: {
+    local $TODO = "This returns a CHI object, which is probably fine.";
+    isa_ok($agent->cache,     'Exobrain::Cache');
+}
 
 done_testing;
