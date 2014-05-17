206b74131c833698f772f3aba09ac0b0312cff57
Leon Wright <techman83@gmail.com>
2014-05-17 14:08:53 +0800

    Logging level/location configurable and part of 'exobrain setup'

--- lib/Exobrain/Logger.pm
+++ lib/Exobrain/Logger.pm
@@ -4,6 +4,7 @@ use Log::Log4perl;
 
 use Moose;
 use Method::Signatures;
+use Exobrain::Config;
 
 # ABSTRACT: Simple Log4Perl access
 # VERSION
@@ -38,15 +39,14 @@ has log => (
 );
 
 method _build_log() {
-  my $logfile = "/home/leon/exobrain.log";       # Load from logger Config file
-  my $loglevel = 'INFO';  # Load from logger config file
+  my $config = Exobrain::Config->new;
 
   # In reality this whole lot could be loaded from a log.conf, but the above may be simpler.
   my $log_conf = qq(
-    log4perl.rootLogger                  = $loglevel, Exobrain
+    log4perl.rootLogger                  = $config->{Logger}{level}, Exobrain
     log4perl.appender.Exobrain           = Log::Log4perl::Appender::File
     log4perl.appender.Exobrain.utf8      = 1
-    log4perl.appender.Exobrain.filename  = $logfile
+    log4perl.appender.Exobrain.filename  = $config->{Logger}{file}
     log4perl.appender.Exobrain.mode      = append
     log4perl.appender.Exobrain.layout    = Log::Log4perl::Layout::PatternLayout
     log4perl.appender.Exobrain.layout.ConversionPattern = %d %p %m %n
@@ -54,6 +54,15 @@ method _build_log() {
 
   Log::Log4perl->init_once(\$log_conf);
   return Log::Log4perl->get_logger();
+}
+
+method setup() {
+    my $config = 
+        "[Logger]\n" .
+        "file       = $ENV{HOME}/ubic/log/exobrain/exobrain.log\n" .
+        "level      = INFO\n";
+
+    my $filename = Exobrain::Config->write_config('Logger.ini', $config);
 }
 
 1;
